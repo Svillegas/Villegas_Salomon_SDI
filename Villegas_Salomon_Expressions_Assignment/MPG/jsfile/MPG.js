633dc0ebd6684b249e72f25c368b0df740b95ee5
Salomon Villegas <salomon2gh@gmail.com>
2015-05-20 08:56:57 -0400

    edited html to source js file and tested miles array within it

--- Villegas_Salomon_Expressions_Assignment/MPG/jsfile/MPG.js
+++ Villegas_Salomon_Expressions_Assignment/MPG/jsfile/MPG.js
@@ -2,4 +2,20 @@
  * Expressions Assignment Created by Salomon Villegas on 5/20/15.
  */
 
+ // Expressions Assignment Created by Salomon Villegas on 5/20/15.
+
+// This calculator will calculate the MPG rating over a Month period.
+
+
+var Miles_Array = [Number(prompt(" Enter Odometer reading for week 1. ")), Number(prompt(" Enter Odometer reading for week 2. ")), Number(prompt(" Enter Odometer reading for week 3. ")),Number(prompt(" Enter Odometer reading for week4. "))];
+
+//User enters readings in the automobile's odometer to prompts.
+
+var Miles = (Miles_Array [0] + Miles_Array [1] + Miles_Array [2] + Miles_Array [3])
+
+console.log (Miles)
+
+
+
+
 
