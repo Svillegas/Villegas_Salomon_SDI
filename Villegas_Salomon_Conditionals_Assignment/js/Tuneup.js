f7865c39a6c2fd76b4913d92dd9eee17959a9cc2
Salomon Villegas <salomon2gh@gmail.com>
2015-05-21 19:13:21 -0400

    Front axle prompts and calculation were added with conditional and else statements proceeding to Rear axle. additionally: many corrections have been made

--- Villegas_Salomon_Conditionals_Assignment/js/Tuneup.js
+++ Villegas_Salomon_Conditionals_Assignment/js/Tuneup.js
@@ -16,7 +16,7 @@ console.log("The optimal tire pressure for the Left front wheel is", Fwheel_L);
 var Rwheel_R = 30;
 console.log("The optimal tire pressure for the Right rear wheel is", Rwheel_R);
 
-var Rwheel_L = 30
+var Rwheel_L = 30;
 console.log("The optimal tire pressure for the Left rear wheel is", Rwheel_R);
 
 // The following values represent the optimal individual tire pressure for the both rear wheels.
@@ -29,19 +29,24 @@ console.log("The total optimal tire pressure for the Front axle is", Front_axle_
 var Rear_axle_optimal = (Rwheel_R + Rwheel_L );
 console.log("The total optimal tire pressure for the Rear axle is", Rear_axle_optimal);
 
-// The total optimal tire pressure for the front axle according to the calculation made in the log is
+// The total optimal tire pressure for the rear axle according to the calculation made in the log is
 
 var User_Name = prompt("Hello! Please enter your name.")
 console.log("The name of the user is", User_Name);
 
+
+
+
+
 // This prompt collects the user's whole name
 
 //---------------------------------------------------------------------------------------------------------//
-//Collecting Name from user through prompt window that allows him to conti
+
+/* This block of code collects name from user through prompt window that allows him to continue or to cancel request*/
 
 if(User_Name = User_Name){
 
-    var r = confirm(("Thank you ") + (User_Name) + (" for using this handy tool \n that will help you check for optimal tire pressure"));
+    var r = confirm(("Thank you ") + (User_Name) + (" for using this handy tool that will help you check for optimal tire pressure."));
     if (r == true) {
         x = "You cancelled the request";
     } else {
@@ -60,16 +65,48 @@ if(User_Name = User_Name){
     if (r == true) {
         x = "You cancelled the request";
     }else{
-        x = alert("You Cancelled the request!")
+        x = alert("You Cancelled the request!") //-----> this alerts user that the request has been cancelled
 
         console.log("user cancelled");
 
         document("syntax error to stop script") //-------> made up function to stop the script if user cancels request
 
-        //this block greets the user after entering name 
+        //this block reminds the user that he did not enter his name
     }
 };
-console.log("user has continued")
+console.log("user has continued");
+
+//----------------------------------------------------------------------------------------------------------//
+
+/*This block of code will ask the user to enter the information for their current tire pressure in the front axle
+and compare it to the optimal value */
+
+var Front_axle_User = [Number(prompt(" Enter pressure reading for front Right wheel. ")) +
+                        Number(prompt(" Enter pressure reading for front Right wheel. "))];
+    console.log(" the total user's tire pressure is ", Front_axle_User)
+
+//This prompt array allows User to enter Reading for both tires on the front axle...
+
+
+if(Front_axle_User == Front_axle_optimal){
+
+    alert("Perfect, The tire pressure on the front end of your car is optimal.")
+        console.log( "user successfully filled and measured tire pressure for the front axle")
+
+    }else {
+
+    var New_reading = prompt(" Hmm... It seems your tire pressure is not set, please check \n" +
+        " your reading one more time and enter the new reading below. ")
+
+    console.log("User has corrected the tire pressure readings for the front axle and its now", New_reading)
+}
+
+
+/* This block of code will ask the user to enter the information for their current tire pressure in the rear axle
+and compare it to the optimal value */
+
+
+
 
 
 
