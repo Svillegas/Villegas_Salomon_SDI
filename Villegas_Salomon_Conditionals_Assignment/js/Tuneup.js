0effeaf40930949b5587bb83ac6cd41ee55ea212
Salomon Villegas <salomon2gh@gmail.com>
2015-05-21 22:08:39 -0400

    added rear axle calculations and corrected front axle codes. To both, I added  conditions in which the script stops if the user doesnt enter anything in the prompts.

--- Villegas_Salomon_Conditionals_Assignment/js/Tuneup.js
+++ Villegas_Salomon_Conditionals_Assignment/js/Tuneup.js
@@ -5,6 +5,7 @@
 //------------------------------------------Variables---------------------------------------------------//
 // Whole Car Tire Pressure optimal
 
+
 var Fwheel_R = 28;
 console.log("The optimal tire pressure for the Right front wheel is", Fwheel_R);
 
@@ -81,29 +82,92 @@ console.log("user has continued");
 /*This block of code will ask the user to enter the information for their current tire pressure in the front axle
 and compare it to the optimal value */
 
-var Front_axle_User = [Number(prompt(" Enter pressure reading for front Right wheel. ")) +
-                        Number(prompt(" Enter pressure reading for front Right wheel. "))];
-    console.log(" the total user's tire pressure is ", Front_axle_User)
+var no_input = 0
+    console.log(no_input);
+
+var Front_axle_User = [Number(prompt(" Enter pressure reading for Front Right wheel. ")) +
+                        Number(prompt(" Enter pressure reading for Front Left wheel. "))];
+    console.log(" the total user's tire pressure is ", Front_axle_User);
+
+
 
 //This prompt array allows User to enter Reading for both tires on the front axle...
 
 
-if(Front_axle_User == Front_axle_optimal){
+if(Front_axle_User >= Front_axle_optimal){
 
     alert("Perfect, The tire pressure on the front end of your car is optimal.")
         console.log( "user successfully filled and measured tire pressure for the front axle")
 
-    }else {
+    }else if(Front_axle_User > no_input){
 
-    var New_reading = prompt(" Hmm... It seems your tire pressure is not set, please check \n" +
-        " your reading one more time and enter the new reading below. ")
+    // if the tire pressure is lower than the optimal the user is asked to recheck and submit again the values.
 
-    console.log("User has corrected the tire pressure readings for the front axle and its now", New_reading)
+    var New_reading = prompt(" Hmm... It seems your Front axle tire pressure is not set. Please check \n" +
+    " your reading one more time and enter the new reading below. ");
+
+console.log("User has corrected the tire pressure readings for the front axle and its now", New_reading)
+
+}else{
+
+    //if nothing is entered even with the warning the script is stopped
+
+    alert(" You still haven't entered anything Restarting tool")
+
+    console.log("User didn't enter anything. Script was stopped")
+
+    document("syntax error to stop script")
+
+
 }
+
+console.log("user continued to Rear axle calculation");
+
+//---------------------------------------------------------------------------------//
+
 
 
 /* This block of code will ask the user to enter the information for their current tire pressure in the rear axle
 and compare it to the optimal value */
+
+
+var no_input = 0
+    console.log(no_input);
+
+var Rear_axle_User = [Number(prompt(" Enter pressure reading for Rear Right wheel. ")) +
+                        Number(prompt(" Enter pressure reading for Rear Left wheel. "))];
+    console.log(" the total user's tire pressure is ", Rear_axle_User);
+
+//This prompt array allows User to enter Reading for both tires on the front axle...
+
+
+if(Rear_axle_User >= Rear_axle_optimal){
+
+    alert("Perfect, The tire pressure on the front end of your car is optimal.")
+    console.log( "user successfully filled and measured tire pressure for the rear axle")
+
+}else if(Rear_axle_User > no_input){
+
+    // if the tire pressure is lower than the optimal the user is asked to recheck and submit again the values.
+
+    var New_reading_2 = prompt(" Hmm... It seems your Front axle tire pressure is not set. Please check \n" +
+        " your reading one more time and enter the new reading below. ");
+
+    console.log("User has corrected the tire pressure readings for the rear axle and its now", New_reading_2)
+
+}else{
+
+    //if nothing is entered even with the warning the script is stopped
+
+    alert(" You still haven't entered anything. Restarting tool")
+
+    console.log("User didn't enter anything. Script was stopped")
+
+    document("syntax error to stop script")
+}
+
+///--------------/--/-//
+
 
 
 
