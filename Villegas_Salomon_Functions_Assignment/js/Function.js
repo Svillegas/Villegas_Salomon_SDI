be2d867edb507ce2ffde7a37feff61613e282d9a
Salomon Villegas <salomon2gh@gmail.com>
2015-05-28 21:43:30 -0400

    added horsepower caluclator with validated prompts and functions

--- Villegas_Salomon_Functions_Assignment/js/Function.js
+++ Villegas_Salomon_Functions_Assignment/js/Function.js
@@ -1,7 +1,6 @@
 /******
  *Functions used for validating form entries
  */
-
 function validation_Req(val) {
     return (val);
 }
@@ -21,7 +20,7 @@ function valLen(val, len) {
 
 
 /******
- *Prompt the user for their name until they enter a valid response
+ *Prompt the user for the vehicle brand until they enter a valid response
  */
 
 var V_Name, valid=false;
@@ -31,7 +30,7 @@ while(!valid) {
 }
 
 /******
- *Prompt the user for their employee number until they enter a valid response
+ *Prompt the user for their VIN until they enter a valid response
  */
 
 var Vehicle_VIN, valid=false;
@@ -51,3 +50,35 @@ console.log(" The User's Vin number is: " +Vehicle_VIN+ ".");
 
 //------------------------------------------------------------------//
 
+//Calculating horse power
+
+
+
+V_HP_total = V_HP(
+    (var V_Name, valid=false;
+while(!valid) {
+    V_Name = prompt("Please enter the torque of your vehicle." , "");
+    valid = (validation_Req(V_Name) && validation_Req_2(V_Name));
+}),
+(var V_Name, valid=false;
+while(!valid) {
+    V_Name = prompt("Please enter the RPMs of your vehicle" , "");
+    valid = (validation_Req(V_Name) && validation_Req_2(V_Name));
+});
+
+)
+
+function V_HP( t, r ){
+
+    var Uni_reading = 5252;
+
+
+    var v_calc = (t * r/ Uni_reading);
+    console.log( "The vehicle's horsepower is " + v_calc + ".");
+
+    return (v_calc);
+}
+
+alert("Your vehicle's horsepower is " + V_HP_total + ".");
+
+//-----------------//
